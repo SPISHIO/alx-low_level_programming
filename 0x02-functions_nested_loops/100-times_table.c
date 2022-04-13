cd228501876daea148808a047e2033b13018b693
SPISHIO <88729599+SPISHIO@users.noreply.github.com>
2022-04-13 03:55:40 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,17 +1,49 @@
-#include "holberton.h"
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
+/*
+ * File: 100-times_table.c
+ * Auth: Brennan D Baraban
  */
-int main(void)
-{
-	char c[] = "Holberton";
-	int i;
 
-	for (i = 0; i < 15; i++)
+#include "holberton.h"
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		_putchar(c[i]);
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
-	return (0);
 }
