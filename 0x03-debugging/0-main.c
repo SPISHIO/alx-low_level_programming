0bc9ca9b970b3a8e77a30922d94202020037e2c0
SPISHIO <88729599+SPISHIO@users.noreply.github.com>
2022-04-15 00:49:18 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,24 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(void)
+int positive_or_negative(void)
 {
-	int i;
+	int n;
 
-	i = 0;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
-}
