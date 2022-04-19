8653bac02f756fb3f09466fac373913763943a07
SPISHIO <88729599+SPISHIO@users.noreply.github.com>
2022-04-19 22:13:19 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,13 @@
-/*
- * File: 2-mul.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
-
 /**
- * mul - Multiples two integers.
- * @a: The first integer to be multiplied.
- * @b: The second integer to be multiplied.
- *
- * Return: The result of the multiplication.
+ * multiply two numbers
+ * return int.
  */
 int mul(int a, int b)
 {
-	return (a * b);
-}
+  int c;
+  c = a * b;
+  
+  return(c);
+   
+ }
